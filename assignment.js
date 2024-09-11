21fbd8784f85f1c344b194e4163ac3edb642f045
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-09-12 03:06:44 +0600

    4 problem solbe

--- assignment.js
+++ assignment.js
@@ -3,16 +3,15 @@
 
 function calculateTax(income, expenses) {
 
-    if (income < 0 || expenses < 0 || expenses >= income) {
+    if (income < 0 || expenses < 0 || income <= expenses) {
         return "Invalid Input";
     }
-
-    let manthlyincomeabg = income - expenses
-    let tax = manthlyincomeabg * .20;
-    // return tax;
+    let manthlyincome  = income - expenses
+    let tax = manthlyincome  * 20 / 100;
+    return tax;
 }
-// let man = calculateTax(10000, 3000);
-// console.log(man);
+let man = calculateTax(10000, 3000);
+console.log(man);
 
 
 
@@ -24,15 +23,14 @@ function sendNotification(email) {
     if (!email.includes("@")) {
         return "Invalid Email"
     }
-    let username = "zihad.ph";
-    let domingname = "gmail.com";
-    // return username + " sent you an email from " + domingname
+    let username = email.slice(0, 8);
+    let domainname = email.slice(9);
+    let notification = username + " " + "sent you an email from" + " " + domainname;
+    return notification;
 }
 
 // let finall = sendNotification("zihad.ph@gmail.com");
 // console.log(finall);
-
-
 
 
 // task 3 start//
@@ -43,14 +41,23 @@ function checkDigitsInName(name) {
     if (typeof name !== "string") {
         return "Invalid Input"
     }
+    for (let i = 0; i < name.length; i++) {
+        let chek = name[i];
 
-    let numberchek = /\d/;
-    return numberchek.test(name);
+        if (chek >= "0" && chek <= "9") {
+            return true;
+        }
+    }
+    return false
 
 }
 
-//  let funal = checkDigitsInName("Raj123")
-//  console.log(funal);
+// let funal = checkDigitsInName("Raj123")
+// console.log(funal);
+
+
+
+
 
 
 
@@ -60,28 +67,26 @@ function checkDigitsInName(name) {
 
 function calculateFinalScore(obj) {
 
-      if(typeof obj !== "object"){
+    if (typeof obj !== "object") {
         return "Invalid Input";
-      }
+    }
 
-      
+    else if (obj.testScore > 50 || obj.schoolGrade > 30) {
+        return "Invalid Input"
+    }
 
-    if (obj.testScore >= 0 || obj.schoolGrade >= 0 || obj.isFFamily === false) {
-        let finalscore = obj.testScore + obj.schoolGrade + man;
-        if (finalscore >= 80) {
-            return true;
-        }
-        else if(obj.isFFamily === true){
-            let man = 80;
-          }
-        else {
-            return false;
-        }
+    if (obj.testScore >= 0 || obj.schoolGrade >= 0 || obj.isFFamily === true) {
+        let finalscore = obj.testScore + obj.schoolGrade + 20;
+        // if (finalscore >= 80) {
+        //     return true;
+        // }
+        // else {
+        //     return false;
+        // }
     }
 
 }
 
-let final = calculateFinalScore({ name: "Rajib", testScore: 45, schoolGrade: 25, isFFamily: true });
-
-// let final = calculateFinalScore(5646);
-console.log(final);
+// let final = calculateFinalScore({ name: "Rajib", testScore: 45, schoolGrade: 25, isFFamily: true });
+// // let final = calculateFinalScore(5646);
+// console.log(final);
